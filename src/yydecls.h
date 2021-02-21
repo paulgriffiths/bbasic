d9aea49e612de6fedd7623a0caab02010cc87a0e
Paul Griffiths <paul@paulgriffiths.net>
2021-02-21 11:55:25 -0500

    Add yyparse declaration to yydecls.h

--- src/yydecls.h
+++ src/yydecls.h
@@ -22,8 +22,9 @@
 #include <stddef.h>
 
 typedef struct yy_buffer_state * YY_BUFFER_STATE;
-extern YY_BUFFER_STATE yy_scan_buffer(char *, size_t);
-extern void yy_delete_buffer(YY_BUFFER_STATE buffer);
-extern void yyrestart(FILE *);
+YY_BUFFER_STATE yy_scan_buffer(char *, size_t);
+void yy_delete_buffer(YY_BUFFER_STATE buffer);
+void yyrestart(FILE *);
+int yyparse(void);
 
 #endif  /* PG_BBASIC_INTERNAL_YYDECLS_H */
